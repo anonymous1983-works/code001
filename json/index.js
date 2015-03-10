1367491371876d59f17054548ea964654a66a803
Anis ABID Amundi <abid@WSV00324842.ad-its.credit-agricole.fr>
2015-03-10 18:02:41 +0100

    delet folder app/ json/

--- json/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-    data = { users: [] }
-    // Create 1000 users
-    for (var i = 0; i < 1000; i++) {
-        data.users.push({ name: 'user' + i })
-    }
-    return data
-}
